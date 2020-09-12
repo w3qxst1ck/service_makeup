fdbcf21ba68acd466875ce077cdcd031c3467013
Alexander Andreev <1996sasha2507@mail.ru>
2020-09-12 10:57:05 +0300

    Finished adding records functional

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 import datetime
 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 import json
 
@@ -34,7 +35,14 @@ def service(request, category_slug, id, slug):
 
     dates = [now + datetime.timedelta(days=i) for i in range(7)
             if (now + datetime.timedelta(days=i)).weekday() not in [5, 6]]
-    times = ['0' + str(i) if len(str(i)) == 1 else str(i) for i in range(8, 18)]
+
+    date1 = dates[0]
+    date2 = dates[1]
+    date3 = dates[2]
+    date4 = dates[3]
+    date5 = dates[4]
+
+    # times = ['0' + str(i) if len(str(i)) == 1 else str(i) for i in range(8, 18)]
     # ordered_times = [str(record.date.time())[:2] for record in Record.objects.filter(service__slug=slug,
     #                                                     date__gte=(now-datetime.timedelta(hours=now.hour)))]
     # ordered = {}
@@ -45,17 +53,41 @@ def service(request, category_slug, id, slug):
     #         ordered[rec.date.day].append(str(rec.date.time())[:2])
     # print(ordered)
 
-    week = []
-    for day in range(now.day, now.day + 1):
-        for hour in range(8, 18):
-            if datetime.datetime(now.year, now.month, day, hour).weekday() not in [5, 6]:
-                week.append(datetime.datetime(now.year, now.month, day, hour, 0, 0, 0, UTC))
-
-    ordering_dates = [rec.date for rec in Record.objects.filter(service__slug=slug,
+    # week = []
+    # for day in range(now.day, now.day + 1):
+    #     for hour in range(8, 18):
+    #         if datetime.datetime(now.year, now.month, day, hour).weekday() not in [5, 6]:
+    #             week.append(datetime.datetime(now.year, now.month, day, hour, 0, 0, 0, UTC))
+    #
+    ordered_dates = [rec.date for rec in Record.objects.filter(service__slug=slug,
                                                                 date__gte=(now-datetime.timedelta(hours=now.hour)))]
 
+    week = []
+    new_week = [[datetime.datetime(now.year, now.month, day, hour, 0, 0, 0, UTC) for hour in range(8, 18)
+                 if datetime.datetime(now.year, now.month, day, hour).weekday() not in [5, 6]]
+                for day in range(now.day, now.day + 7)]
+    for el in new_week:
+        if el:
+            week.append(el)
 
-    context = {'service': service, 'category': category, 'week': week, 'dates': dates, 'times': times}
+    day1 = week[0]
+    day2 = week[1]
+    day3 = week[2]
+    day4 = week[3]
+    day5 = week[4]
+
+    context = {'service': service, 'category': category, 'week': week, 'ordered_dates': ordered_dates,
+               'day1': day1,
+               'day2': day2,
+               'day3': day3,
+               'day4': day4,
+               'day5': day5,
+               'date1': date1,
+               'date2': date2,
+               'date3': date3,
+               'date4': date4,
+               'date5': date5}
+
     return render(request, 'core/services_in_category.html', context)
 
 
@@ -69,6 +101,7 @@ def add_record(request):
     if not Record.objects.filter(service=service_id, date=date):
         Record.objects.create(user=request.user, service=service, date=date)
 
+    # messages.info(request, "Вы были записаны на выбранную дату.")
     return JsonResponse('Record was added', safe=False)
 
 
